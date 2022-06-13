c65e5148a8022d82a017dbc115411479f7ad869a
schung <schung@nuvoton.com>
2022-06-13 14:22:21 +0800

    Update dcultrafb to fix ts_calibrate not showing
    Change-Id: Ib17fbfaa8ee8e76b72321a0f948f73225251374c

--- meta-ma35d1/recipes-graphics/qt5-env/files/qt5-env.sh
+++ meta-ma35d1/recipes-graphics/qt5-env/files/qt5-env.sh
@@ -4,4 +4,5 @@ export QT_QPA_FONTDIR=/usr/share/fonts/truetype
 export XDG_RUNTIME_DIR="/tmp/runtime-root"
 export QT_QPA_PLATFORM="linuxfb"
 export QT_QPA_FB_TSLIB=1
+export TSLIB_CONSOLEDEVICE=none
 
